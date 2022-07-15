c7108e132ac797c0e7ddea6edafeccd12ab19cf0
Ayaan <kaifullah7921@gmail.com>
2022-07-15 16:17:25 +0530

    fresh restarting of project

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => {
-    return <div>Hello Latest jsx World</div>;
-};
-ReactDOM.render(<App />, document.getElementById("app"));
-// console.log("Hello New world");
--- README.md
+++
@@ -1,2 +0,0 @@
-# Hangman
-Restarting the project
